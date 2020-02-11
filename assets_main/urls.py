96cda5468d9eec3d877f7e47bfbc8b496587aec0
sewookJung <tpdnrqkqh@naver.com>
2020-02-11 13:04:59 +0900

    deleted rent page

--- assets_main/urls.py
+++ assets_main/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^rent/status', views.assets_status, name="assets_status"),
-    url(r'^rent/', views.assets_rent, name="assets_rent"),
     url(r'^add/', views.assets_add, name="assets_add"),
     url('', views.assets_main, name="assets_main"),
 ]
