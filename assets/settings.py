cbc6373a4b43a81840ce722b3e339cd3b7a89aa2
sewookJung <tpdnrqkqh@naver.com>
2020-02-12 17:30:07 +0900

    added login and member app

--- assets/settings.py
+++ assets/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'assets_main',
-    'document_main'
+    'document_main',
+    'login',
+    'member'
 ]
 
 MIDDLEWARE = [
