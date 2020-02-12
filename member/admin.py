d2e6c40fc59b1b45e2124f9f1cec68f2dc9b2f64
sewookJung <tpdnrqkqh@naver.com>
2020-02-12 17:11:33 +0900

    added display element

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Member
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['id', 'member_id',
+                    'member_name', 'member_dept', 'member_rank', 'member_status']
+    list_display_links = ['id', 'member_id', 'member_name']
+
+
+admin.site.register(Member, MemberAdmin)
 
 # Register your models here.
