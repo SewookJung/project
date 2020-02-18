c7ac1f666f1905beadd526ba748b06032334ff9d
sewookJung <tpdnrqkqh@naver.com>
2020-02-18 16:29:12 +0900

    added assets and assetsrent model

---
+++ assets/models.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from member.models import Member
+
+
+class Asset(models.Model):
+
+    # 'glowellsystem' - 테스트용, 폐기의 경우 변경된다.
+    member_name = models.ForeignKey(
+        Member, null=True, blank=True, on_delete=models.SET_NULL)
+
+    mnfacture = models.CharField(max_length=30, default='')  # 제조사명
+
+    model = models.CharField(max_length=40, default='')  # 모델명
+
+    cpu = models.CharField(max_length=30, default='')  # CPU
+
+    memory = models.CharField(max_length=30, default='')  # MEMORY
+
+    harddisk = models.CharField(max_length=30, default='')  # HARDDISK
+
+    is_where = models.CharField(max_length=30, default='')  # 사용처
+
+    # 현재상태 1: 개인사용, 2: TEST, 3: 대여중, 4:폐기 5: 보관
+    is_state = models.IntegerField(max_length=1, default='')
+
+    purchase_date = models.DateField(null=True, blank=True)  # 구입일
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)  # 등록날짜
+
+    closed = models.DateField(null=True, blank=True)  # 폐기일
+
+    comments = models.CharField(max_length=200, default='')  # 기타 comment
+
+
+class Assetrent(models.Model):
+
+    asset = models.ForeignKey(to=Asset, on_delete=models.SET_NULL, null=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)  # 생성일
+
+    stdate = models.DateField()  # 대여일
+
+    eddate = models.DateField()  # 반납예정일
+
+    member_id = models.CharField(max_length=20, default='')  # 빌린 member id
+
+    return_date = models.DateField(null=True, blank=True)  # 반납일
+
+    comments = models.CharField(max_length=200, default='')  # 기타
