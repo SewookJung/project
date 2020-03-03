849e6ddaf000bbfde1f7c8701ede200823d27be3
sewookJung <tpdnrqkqh@naver.com>
2020-03-03 11:35:13 +0900

    modified of /rent/ url pattern

--- assets/urls.py
+++ assets/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import url
 from . import views
+from django.urls import path, re_path
 
 
 urlpatterns = [
-    url(r'^rent/status', views.assets_status, name="assets_status"),
-    url(r'^add/', views.assets_add, name="assets_add"),
-    url('', views.assets_main, name="assets_main"),
+    path('', views.assets_main, name="assets_main"),
+    path('add/', views.assets_add, name="assets_add"),
+    path('add/apply', views.assets_add_apply, name="asset_add_apply"),
+    path('status/', views.assets_status, name="assets_status"),
+    path('status/apply/', views.assets_rent, name="assets_rent"),
+    path('status/return/',
+         views.assets_return, name="assets_return"),
 ]
