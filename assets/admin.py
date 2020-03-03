413c9405d0248e93362c32ef2a549e120b0b2e03
sewookJung <tpdnrqkqh@naver.com>
2020-03-03 10:51:36 +0900

    changed column name

--- assets/admin.py
+++ assets/admin.py
@@ -3,14 +3,14 @@ from .models import Asset, Assetrent
 
 
 class AssetAdmin(admin.ModelAdmin):
-    list_display = ['id', 'member_id',
+    list_display = ['id', 'member_name',
                     'mnfacture', 'model', 'cpu', 'memory', 'harddisk', 'closed', 'comments']
-    list_display_links = ['id', 'member_id']
+    list_display_links = ['id', 'member_name']
 
 
 class AssetrentAdmin(admin.ModelAdmin):
     list_display = ['id', 'asset',
-                    'created', 'stdate', 'eddate', 'member_id', 'return_date', 'comments']
+                    'created', 'stdate', 'eddate', 'member_name', 'return_date', 'comments']
     list_display_links = ['id', 'asset']
 
 
