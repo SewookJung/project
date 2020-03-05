cea7b299ae304cab33ac439d377a34899867e4fd
sewookJung <tpdnrqkqh@naver.com>
2020-03-05 10:29:32 +0900

    delete

--- assets/urls.py
+++ assets/urls.py
@@ -1,13 +1,13 @@
 from . import views
-from django.urls import path, re_path
+from django.urls import re_path
 
 
 urlpatterns = [
-    path('', views.assets_main, name="assets_main"),
-    path('add/', views.assets_add, name="assets_add"),
-    path('add/apply', views.assets_add_apply, name="asset_add_apply"),
-    path('status/', views.assets_status, name="assets_status"),
-    path('status/apply/', views.assets_rent, name="assets_rent"),
-    path('status/return/',
-         views.assets_return, name="assets_return"),
+    re_path(r'^$', views.assets_main, name="assets_main"),
+    re_path(r'^add/$', views.assets_add, name="assets_add"),
+    re_path(r'^add/apply$', views.assets_add_apply, name="asset_add_apply"),
+    re_path(r'^status/$', views.assets_status, name="assets_status"),
+    re_path(r'^status/apply/$', views.assets_rent, name="assets_rent"),
+    re_path(r'^status/return/$',
+            views.assets_return, name="assets_return"),
 ]
