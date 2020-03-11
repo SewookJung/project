6dfdba859a86f77ad60f46e270f2291466eb0870
sewookJung <tpdnrqkqh@naver.com>
2020-03-11 13:31:09 +0900

    Declare models

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Project, Document, DocumentAttachment
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'client', 'product', 'model',
+                    'status', 'created_at', 'update_at', 'comments']
+    list_display_links = ['id', 'title']
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id',  'project_id', 'member_id',
+                    'kind', 'created_at', 'comments']
+    list_display_links = ['id', 'project_id']
+
+
+class DocumentAttachmentAdmin(admin.ModelAdmin):
+    list_display = ['id',  'document', 'attach_name',
+                    'upload_name', 'upload_dir', 'created_at']
+    list_display_links = ['id', 'document']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(DocumentAttachment, DocumentAttachmentAdmin)
