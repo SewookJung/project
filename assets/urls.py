79d7e10ce0d25a640182146b600760f476f2a145
sewookJung <tpdnrqkqh@naver.com>
2020-03-11 10:31:40 +0900

    added rent of status url

--- assets/urls.py
+++ assets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     re_path(r'^$', views.assets_main, name="assets_main"),
     re_path(r'^add/$', views.assets_add, name="assets_add"),
     re_path(r'^add/apply$', views.assets_add_apply, name="asset_add_apply"),
-    re_path(r'^status/$', views.assets_status, name="assets_status"),
+    re_path(r'^status/rent/$', views.assets_status, name="assets_status"),
     re_path(r'^status/apply/$', views.assets_rent, name="assets_rent"),
     re_path(r'^status/return/$',
             views.assets_return, name="assets_return"),
