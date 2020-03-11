9cc162ca0e6bce31675b9b2bb2ef2d9f2b8e773e
sewookJung <tpdnrqkqh@naver.com>
2020-03-11 10:38:45 +0900

    added meta options and modified lengths

--- common/models.py
+++ common/models.py
@@ -10,21 +10,35 @@ class Dept(models.Model):
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=20, default='')
+    name = models.CharField(max_length=40, default='')
     similiar_word = JSONField(default=dict, blank=True,
                               null=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=20, default='')
-    makers = models.CharField(max_length=20, default='')
-    name = models.CharField(max_length=20, default='')
+    name = models.CharField(max_length=40, default='')
+    makers = models.CharField(max_length=40, default='')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class ProductModel(models.Model):
-    name = models.CharField(max_length=20, default='')
+    name = models.CharField(max_length=40, default='')
     product_id = models.ForeignKey(
         Product, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.name
