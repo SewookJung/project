52254ab7b1331da81a4ff41bed8ebc5fc8805980
sewookJung <tpdnrqkqh@naver.com>
2020-03-19 15:39:07 +0900

    Separated datatable

--- assets/static/js/datatable/dataTable.js
+++ assets/static/js/datatable/dataTable.js
@@ -12,7 +12,20 @@ $(document).ready(function() {
     }
   });
 
-  $("#myTable1, #myTable2").DataTable({
+  $("#myTable1").DataTable({
+    language: {
+      paginate: {
+        previous: "‹",
+        next: "›"
+      }
+    }
+  });
+
+  $("#sites_table").DataTable({
+    columnDefs: [
+      { orderable: true, targets: [0] },
+      { orderable: false, targets: "_all" }
+    ],
     language: {
       paginate: {
         previous: "‹",
