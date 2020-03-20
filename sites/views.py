834ddcd2ef9eb5c1d1ec3de29a8ff6eecd8d8ca4
sewookJung <tpdnrqkqh@naver.com>
2020-03-20 11:21:42 +0900

    Sites views
    1. Make response object
     - This object is returning response
     - This object is using of document_upload_apply function
     - This object was made for using finupload(jquery library)
     - Fineupload is need to response
    
    2. Document upload object
     - Added csrf_exempt module
     - This object is refers to make_response object

--- sites/views.py
+++ sites/views.py
@@ -1,27 +1,88 @@
+import json
+
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from assets.forms import AssetForm
-from .forms import ProjectForm
-from .models import Project
-# Create your views here.
+from .forms import ProjectForm, DocumentAttachmentForm, DocumentForm
+from .models import Document, DocumentAttachment, Project
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+
+def make_response(status=200, content_type="text/plain", content=None):
+    response = HttpResponse()
+    response.status_code = status
+    response['Content-Type'] = content_type
+    response.content = content
+    print(response)
+    return response
 
 
 def sites_main(request):
-    return render(request, 'sites/sites_main.html', {})
+    projects = Project.objects.all()
+    return render(request, 'sites/sites_main.html', {"projects": projects})
 
 
 def sites_add(request):
     asset_form = AssetForm()
     project_form = ProjectForm()
-    return render(request, "sites/sites_add.html", {'asset_form': asset_form, 'project_form': project_form})
+    document_form = DocumentAttachmentForm()
+    return render(request, "sites/sites_add.html", {'asset_form': asset_form, 'project_form': project_form, 'document_form': document_form, })
 
 
 def sites_add_apply(request):
     if request.method == "POST":
-        project_apply = Project(title=request.POST['project_name'], status=request.POST['status'], comments=request.POST['comments'],
-                                client_id=request.POST['client_id'], product_id=request.POST[
-                                    'product_id'],  model_id=request.POST['model_id'],
-                                info={"sales": request.POST['sales'], "started_at": request.POST['pjStDate'], "ended_at": request.POST['pjEdDate'], "msstarted_at": request.POST['mnStDate'], "mended_at": request.POST['mnEdDate'], "eng": request.POST['eng']})
+        print(request.POST)
+        project_apply = Project(title=request.POST['project_name'], status=request.POST['status'],
+                                comments=request.POST['sites_comments'], client_id=request.POST[
+                                    'client'], product_id=request.POST['product'],
+                                info={"sales": request.POST.getlist('member_name')[0], "mn_cycle": request.POST['cycle'], "eng": request.POST.getlist('member_name')[1], "started_at": request.POST.getlist('purchase_date')[
+                                    0], "ended_at": request.POST.getlist('purchase_date')[1], "msstarted_at": request.POST.getlist('purchase_date')[2], "mended_at": request.POST.getlist('purchase_date')[3]}
+                                )
         project_apply.save()
-        return redirect('sites_main')
+        return redirect("sites_main")
     else:
-        return render(request, "sites/sites_add.html", {})
+        return redirect("sites_main")
+
+
+def document_upload(request):
+    document_form = DocumentForm()
+    return render(request, "sites/sites_upload.html", {'document_form': document_form})
+
+
+@csrf_exempt
+def document_upload_apply(request):
+    if request.method == "POST":
+        print(request.POST)
+        print(request.FILES)
+
+        document_search = Document.objects.filter(
+            project_id=request.POST['project'])
+
+        if not document_search:
+            try:
+                document_apply = Document(
+                    project_id=request.POST['project'], member_id=9, kind=request.POST['kind'])
+                document_apply.save()
+                document_attach_apply = DocumentAttachment(
+                    attach_name=request.POST['qqfilename'], upload_name=request.POST['qqfilename'], upload_dir="test", document_id=document_apply.id, attach=request.FILES['qqfile']
+                )
+                document_attach_apply.save()
+                return make_response(content=json.dumps({'success': True}))
+            except:
+                return make_response(status=400, content=json.dumps({'success': False, 'error': "error message to display"}))
+
+        else:
+            try:
+                document = Document.objects.filter(
+                    project_id=request.POST['project'])
+
+                document_attach_apply = DocumentAttachment(
+                    attach_name=request.POST['qqfilename'], upload_name=request.POST['qqfilename'], upload_dir="test", document_id=document[0].id, attach=request.FILES['qqfile']
+                )
+                document_attach_apply.save()
+                return make_response(content=json.dumps({'success': True}))
+            except:
+                return make_response(status=400, content=json.dumps({'success': False, 'error': "error message to display"}))
+    else:
+        return redirect("sites_main")
