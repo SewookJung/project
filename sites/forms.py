dea422f402caa4d1495fd6d4c3559caaf044f2f3
sewookJung <tpdnrqkqh@naver.com>
2020-03-20 10:40:22 +0900

    Added forms

--- sites/forms.py
+++ sites/forms.py
@@ -1,15 +1,36 @@
 from django import forms
-from .models import Project
+from .models import Project, Document, DocumentAttachment
 
 
 class ProjectForm(forms.ModelForm):
 
     class Meta:
         model = Project
-        fields = ('client', 'product', 'model', 'status')
+        fields = ('client', 'product', 'status')
         widgets = {
-            'client': forms.Select(attrs={'class': 'selectpicker', 'title': '고객사 선택', 'id': 'client_id'}),
+            'client': forms.Select(attrs={'class': 'selectpicker', 'title': '고객사 선택', 'id': 'client_id', 'data-live-search': 'true'}),
             'product': forms.Select(attrs={'class': 'selectpicker', 'title': '제품 선택', 'id': 'product_id'}),
-            'model': forms.Select(attrs={'class': 'selectpicker', 'title': '제품 선택', 'id': 'model_id'}),
             'status': forms.Select(attrs={'class': 'selectpicker', 'title': '진행 여부 선택', 'id': 'status'})
         }
+
+
+class DocumentForm(forms.ModelForm):
+
+    class Meta:
+        model = Document
+        fields = ('project', 'kind')
+        widgets = {
+            'project': forms.Select(attrs={'class': 'selectpicker',  'id': 'project'}),
+            'kind': forms.Select(attrs={'class': 'selectpicker',  'id': 'kind'}),
+        }
+
+
+class DocumentAttachmentForm(forms.ModelForm):
+
+    class Meta:
+        model = DocumentAttachment
+        fields = ['attach']
+
+    def __init__(self, *args, **kwargs):
+        super(DocumentAttachmentForm, self).__init__(*args, **kwargs)
+        self.fields['attach'].required = False
