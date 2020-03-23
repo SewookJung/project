419053463846130d504cae049a423dc93b0ad041
sewookJung <tpdnrqkqh@naver.com>
2020-03-23 19:14:46 +0900

    Added tootip of bootstrap

---
+++ static/js/sites/tooltip.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('[data-toggle="tooltip_presales"]').tooltip({
+    title:
+      "<div>[삼성디스플레이] NAC 제품소개서</div><div>[삼성디스플레이] GPI 제품소개서</div>",
+    html: true
+  });
+  $('[data-toggle="tooltip_wbs"]').tooltip({
+    title: "<div>[삼성디스플레이] WBS</div>",
+    html: true
+  });
+  $('[data-toggle="tooltip_confirm"]').tooltip({
+    title:
+      "<div>[삼성디스플레이] 검수확인서 1.0V</div><div>[삼성디스플레이] 검수확인서 1.1</div>",
+    html: true
+  });
+});
