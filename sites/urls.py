093e705e22ee8a27ba90d44d28a641bca3bd24c6
sewookJung <tpdnrqkqh@naver.com>
2020-03-23 18:15:57 +0900

    Added detail, edit urls

--- sites/urls.py
+++ sites/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import re_path
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,6 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^$', views.sites_main, name="sites_main"),
+    re_path(r'^(?P<pk>\d+)/$', views.sites_detail, name="sites_detail"),
+    re_path(r'^(?P<pk>\d+)/edit/$', views.sites_edit, name="sites_edit"),
     re_path(r'^add/$', views.sites_add, name="sites_add"),
     re_path(r'^add/apply/$', views.sites_add_apply, name="sites_add_apply"),
     re_path(r'^document/upload/$',
