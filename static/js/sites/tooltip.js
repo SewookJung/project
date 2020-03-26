9c94410388d82d2e6a05dc21e3e7088329a79989
sewookJung <tpdnrqkqh@naver.com>
2020-03-26 10:47:48 +0900

    Added popover function

--- static/js/sites/tooltip.js
+++ static/js/sites/tooltip.js
@@ -1,9 +1,4 @@
 $(function() {
-  $('[data-toggle="tooltip_presales"]').tooltip({
-    title:
-      "<div>[삼성디스플레이] NAC 제품소개서</div><div>[삼성디스플레이] GPI 제품소개서</div>",
-    html: true
-  });
   $('[data-toggle="tooltip_wbs"]').tooltip({
     title: "<div>[삼성디스플레이] WBS</div>",
     html: true
@@ -12,5 +7,20 @@ $(function() {
     title:
       "<div>[삼성디스플레이] 검수확인서 1.0V</div><div>[삼성디스플레이] 검수확인서 1.1</div>",
     html: true
+  });
+});
+
+$(document).ready(function() {
+  var content = [
+    '<div class="timePickerCanvas"><a href="#">NAC 제품소개서</a></div>',
+    '<div class="timePickerClock timePickerHours"><a href="#">EDR 제품소개서</a></div>',
+    '<div class="timePickerClock timePickerMinutes"><a href="#">GPI 제품소개서</a></div>'
+  ].join("");
+  console.log(content);
+
+  $('[data-toggle="popover_presales"]').popover({
+    trigger: "focus",
+    html: true,
+    content: content
   });
 });
