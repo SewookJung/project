11fb32ba3377a0d7a7515bf470f07800ead03071
sewookJung <tpdnrqkqh@naver.com>
2020-04-16 17:30:23 +0900

    Added manafacture admin

--- common/admin.py
+++ common/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dept, Client, Product, ProductModel
+from .models import Dept, Client, Product, ProductModel, Mnfacture
 
 
 class DeptAdmin(admin.ModelAdmin):
@@ -22,7 +22,13 @@ class ProductModelAdmin(admin.ModelAdmin):
     list_display_links = ['id', 'name']
 
 
+class MnfactureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'manafacture', 'created_at']
+    list_display_links = ['id', 'manafacture']
+
+
 admin.site.register(Dept, DeptAdmin)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductModel, ProductModelAdmin)
+admin.site.register(Mnfacture, MnfactureAdmin)
