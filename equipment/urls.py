4c3f12dd72b64429c3410942e975d39152de6623
sewookJung <tpdnrqkqh@naver.com>
2020-04-16 17:37:47 +0900

    Added equipment urls
    1. Root url
     - Root url is main page of equipment
    
    2. Upload url
     - Upload url is upload of file
     - File extension list [.xls, .xlsx]

---
+++ equipment/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import re_path
+from . import views
+
+
+urlpatterns = [
+    re_path(r'^$', views.equipment_main, name="equipment_main"),
+    re_path(r'^upload/', views.equipment_upload, name="equipment_upload"),
+]
