6bf2660ad956def8e8f61b8b9dd6771b516e385c
sewookJung <tpdnrqkqh@naver.com>
2020-04-20 11:01:31 +0900

    Added equipment models
    1. Equipment
     - This model reads the data for sheet1 of uploaded Excel file and sotres it in the database
     - The upload file should be excel's extension
    
    2. EquipmentAttachment
     - This model saves the file

---
+++ equipment/models.py
@@ -0,0 +1,58 @@
+import datetime
+import uuid
+
+from django.db import models
+from django.dispatch import receiver
+from member.models import Member
+from common.models import Client, Product, ProductModel, Mnfacture
+
+
+def _equipmentattachment_upload_path(instance, filename):
+    dt = datetime.datetime.now()
+    filepath = dt.strftime('%Y/%m/')
+    return 'files/{0}/{1}'.format(filepath, uuid.uuid4().hex)
+
+
+class Equipment(models.Model):
+    client = models.ForeignKey(
+        to=Client, null=True, blank=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(
+        to=Product, null=True, blank=True, on_delete=models.SET_NULL)
+    product_model = models.ForeignKey(
+        to=ProductModel, null=True, blank=True, on_delete=models.SET_NULL)
+    mnfacture = models.ForeignKey(
+        to=Mnfacture, null=True, blank=True, on_delete=models.SET_NULL)
+
+    serial = models.CharField(max_length=50, default="")
+    location = models.CharField(max_length=100, default="")
+    comments = models.CharField(max_length=200, default='', blank=True)
+    install_date = models.DateField(max_length=20, blank=True)
+    install_member = models.CharField(max_length=50, default="")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class EquipmentAttachment(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
+    attach = models.FileField(
+        upload_to=_equipmentattachment_upload_path, default="")
+    attach_name = models.CharField(max_length=50, default="")
+    content_size = models.CharField(max_length=50, default="")
+    content_type = models.CharField(max_length=100, default="")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+@receiver(models.signals.pre_save, sender=EquipmentAttachment)
+def equipmentattachment_on_pre_save(sender, instance, *args, **kwargs):
+    if instance.pk:
+        try:
+            obj = sender.objects.get(pk=instance.pk)
+            if obj.attach != instance.attach_name:
+                obj.attach.delete(save=False)
+        except sender.DoesNotExist:
+            pass
+
+
+@receiver(models.signals.post_delete, sender=EquipmentAttachment)
+def equipmentattachment_on_post_delete(sender, instance, *args, **kwargs):
+    if instance.attach:
+        instance.attach.delete(save=False)
