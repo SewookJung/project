7e9c34b89971775889db99efe18d8dcca309673d
sewookJung <tpdnrqkqh@naver.com>
2020-04-20 11:02:39 +0900

    Added equipment urls

--- equipment/urls.py
+++ equipment/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.equipment_main, name="equipment_main"),
+    re_path(r'^form/', views.equipment_form, name="equipment_form"),
+    re_path(r'^input/', views.equipment_input, name="equipment_input"),
+    re_path(r'^bulk/', views.equipment_bulk, name="equipment_bulk"),
+    re_path(r'^bulk_check/', views.equipment_bulk_check, name="equipment_bulk_check"),    
     re_path(r'^upload/', views.equipment_upload, name="equipment_upload"),
 ]
