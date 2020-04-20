caff4ea2ea13ece4198a1a232063d22e7226a041
sewookJung <tpdnrqkqh@naver.com>
2020-04-20 09:37:44 +0900

    Added level field

--- common/models.py
+++ common/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     name = models.CharField(max_length=40, default='')
     makers = models.CharField(max_length=40, default='')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    level = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
