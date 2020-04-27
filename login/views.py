4faf4c417c550d46a55b76c8fbfa535aabb8e01c
sewookJung <tpdnrqkqh@naver.com>
2020-04-27 19:23:17 +0900

    Login views
    1. login function
     - If didn't logined redirect login page logic
    
    2. login_process function
     - First, I used request.session function and save the id, name etc...
     - Second, I used the auth object and the user's authentication uses authedicate.
    
    3. logout function
     - Added redirect logic to login page when request session is none
     - If logout and deleted request session and used auth's logout function
    
    3. redirect_root function
     - If you typed the urls without login and you will redirect login page

--- login/views.py
+++ login/views.py
@@ -1,38 +1,67 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import auth
 from member.models import Member
 # Create your views here.
 
 
 def login(request):
-    msg = ""
-    return render(request, "login/login.html", {"message":msg})
+    if request.method == "GET":
+        try:
+            if request.session.get("member_id", None) == None:
+                msg = ""
+                return render(request, "login/login.html", {"message": msg})
+            else:
+                return redirect('assets_main')
+        except:
+            return redirect('assets_main')
+    else:
+        return render(request, "login/login.html", {})
 
 
 def login_process(request):
-    try:
-        member_info = Member.objects.get(member_id=request.POST['member_id']) # Member 테이블에서 로그인 계정과 일치하는 데이터를 추출
-        if member_info.pw == request.POST['member_pw']: # 사용자가 입력한 비밀번호와 Member 테이블에 등록된 비밀번호가 일치하는 경우
-            request.session['member_id'] = member_info.member_id # 사용자 계정을 저장
-            request.session['member_name'] = member_info.name # 사용자 계정의 이름을 저장
-            request.session['member_dept'] = member_info.dept # 사용자 계정의 부서정보를 저장
+    if request.method == "POST":
+        username = request.POST['member_id']
+        password = request.POST['member_pw']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            member_info = Member.objects.get(
+                member_id=request.POST['member_id'])
+            request.session['id'] = member_info.id
+            request.session['member_id'] = member_info.member_id  # 사용자 계정을 저장
+            request.session['member_name'] = member_info.name  # 사용자 계정의 이름을 저장
+            # 사용자 계정의 부서정보를 저장
+            request.session['member_dept'] = member_info.dept
             request.session['member_rank'] = member_info.rank
-            return redirect('/assets')
-        else: # 사용자가 입력한 비밀번호와 Member 테이블에 등록된 비밀번호가 불일치하는 경우 아래의 메시지를 리턴
-             msg = "계정 또는 비밀번호가 일치하지 않습니다."
-             return render(request, "login/login.html", {"message":msg})
-    except Member.DoesNotExist as msg: # DoesNotExist 오류 발생시 아래의 메시지를 리턴
+            auth.login(request, user)
+            return redirect("assets_main")
+        else:
+            msg = "계정 또는 비밀번호가 일치하지 않습니다."
+            return render(request, "login/login.html", {"message": msg})
+    else:
         msg = "계정 또는 비밀번호가 일치하지 않습니다."
-        return render(request, "login/login.html", {"message":msg})
+        return render(request, "login/login.html", {"message": msg})
+
 
 def logout(request):
-    try:
-        del request.session['member_id'] # 사용자 계정 세션값 삭제
-        del request.session['member_name'] # 사용자 이름 삭제
-        del request.session['member_dept'] # 사용자 부서값 삭제
-        del request.session['member_rank'] 
-    except KeyError:
-        pass
-    msg = ""
-    return render(request, "login/login.html", {"message":msg})
+    if request.method == "GET":
+        if request.session.get("member_id", None) == None:
+            return redirect("login")
+        else:
+            try:
+                del request.session['member_id']  # 사용자 계정 세션값 삭제
+                del request.session['member_name']  # 사용자 이름 삭제
+                del request.session['member_dept']  # 사용자 부서값 삭제
+                del request.session['member_rank']
+                auth.logout(request)
+                return redirect("login")
+            except KeyError:
+                return redirect("login")
+            return redirect("login")
 
+    else:
+        return redirect("login")
+
+
+def redirect_root(request):
+    return redirect('login')
