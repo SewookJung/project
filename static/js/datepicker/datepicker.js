01a354c717cb9f59faf19866fea6caff277861d9
sewookJung <tpdnrqkqh@naver.com>
2020-05-06 17:00:50 +0900

    Added assets datepicker

--- static/js/datepicker/datepicker.js
+++ static/js/datepicker/datepicker.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
   $(
-    "#datepicker1, #datepicker2, #mnStDate, #mnEdDate, #pjStDate, #pjEdDate"
+    "#datepicker1, #datepicker2, #mnStDate, #mnEdDate, #pjStDate, #pjEdDate, #report_date, #rent_date, #return_date"
   ).datepicker({
     format: "yyyy-mm-dd", //데이터 포맷 형식(yyyy : 년 mm : 월 dd : 일 )
     autoclose: true, //사용자가 날짜를 클릭하면 자동 캘린더가 닫히는 옵션
@@ -8,12 +8,12 @@ $(function() {
     daysOfWeekHighlighted: [3], //강조 되어야 하는 요일 설정
     templates: {
       leftArrow: "&laquo;",
-      rightArrow: "&raquo;"
+      rightArrow: "&raquo;",
     }, //다음달 이전달로 넘어가는 화살표 모양 커스텀 마이징
     showWeekDays: true, // 위에 요일 보여주는 옵션 기본값 : true
     todayHighlight: true, //오늘 날짜에 하이라이팅 기능 기본값 :false
     language: "ko", //달력의 언어 선택, 그에 맞는 js로 교체해줘야한다.
     todayBtn: "linked",
-    showMonthAfterYear: true
+    showMonthAfterYear: true,
   }); //datepicker end
 }); //ready end
