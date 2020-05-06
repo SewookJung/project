208b291eff22e6232cf327873ab4275766d4b10d
sewookJung <tpdnrqkqh@naver.com>
2020-05-06 16:59:57 +0900

    Added weekly datatable

--- static/js/datatable/dataTable.js
+++ static/js/datatable/dataTable.js
@@ -1,36 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#asset__table").DataTable({
     order: [
       [3, "asc"],
-      [0, "asc"]
+      [0, "asc"],
     ],
     language: {
       paginate: {
         previous: "‹",
-        next: "›"
-      }
-    }
+        next: "›",
+      },
+    },
   });
 
   $("#myTable1").DataTable({
     language: {
       paginate: {
         previous: "‹",
-        next: "›"
-      }
-    }
+        next: "›",
+      },
+    },
   });
 
   $("#sites_table").DataTable({
     columnDefs: [
       { orderable: true, targets: [0] },
-      { orderable: false, targets: "_all" }
+      { orderable: false, targets: "_all" },
     ],
     language: {
       paginate: {
         previous: "‹",
-        next: "›"
-      }
-    }
+        next: "›",
+      },
+    },
+  });
+
+  $("#weekly_table").DataTable({
+    pageLength: 15,
+    lengthMenu: [5, 10, 15, 20, 30, 50, 100],
+    columnDefs: [
+      { orderable: true, targets: [0] },
+      { orderable: false, targets: "_all" },
+    ],
+    language: {
+      paginate: {
+        previous: "‹",
+        next: "›",
+      },
+    },
   });
 });
