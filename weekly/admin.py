6f99d88672685d08453775f7a8ec83acb84d795c
sewookJung <tpdnrqkqh@naver.com>
2020-05-06 16:22:46 +0900

    Added reportadmin

--- weekly/admin.py
+++ weekly/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'client', 'product',
+                    'member', 'client_manager', 'sales_type', 'support_comment', 'support_date', 'comments', 'created_at', 'update_at']
+    list_display_links = ['id', 'client']
+
+
+admin.site.register(Report, ReportAdmin)
