385ad77a4d53b8a61fa7dfac55804c91628b0673
sewookJung <tpdnrqkqh@naver.com>
2020-05-06 16:15:26 +0900

    Added urls
    1. Edit url
     - Modified edit url before pk value
    
    2. Document detail url
     - Added document detail url
    
    3. Document reg apply url
     - Added document reg apply
     - This url is using for created new document
    
    4. Document download url
     - This url is using for download document

--- sites/urls.py
+++ sites/urls.py
@@ -1,19 +1,23 @@
 from django.conf.urls import re_path
-from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
 
 urlpatterns = [
     re_path(r'^$', views.sites_main, name="sites_main"),
     re_path(r'^(?P<pk>\d+)/$', views.sites_detail, name="sites_detail"),
-    re_path(r'^(?P<pk>\d+)/edit/$', views.sites_edit, name="sites_edit"),
+    re_path(r'^edit/(?P<pk>\d+)/$', views.sites_edit, name="sites_edit"),
     re_path(r'^add/$', views.sites_add, name="sites_add"),
     re_path(r'^add/apply/$', views.sites_add_apply, name="sites_add_apply"),
+    re_path(r'^document/(?P<project_id>\d+)/$',
+            views.document_detail, name="document_detail"),
     re_path(r'^document/upload/$',
             views.document_upload, name="document_upload"),
+    re_path(r'^document/reg/$',
+            views.document_reg_apply, name="document_reg_apply"),
     re_path(r'^document/upload/apply/$', views.document_upload_apply,
             name="document_upload_apply"),
-
+    re_path(r'^document/download/(?P<pk>\d+)/$',
+            views.document_download, name="document_download"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
