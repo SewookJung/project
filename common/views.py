0aaf78c4c33e37e0188d39bed709eafb291fe7e8
sewookJung <tpdnrqkqh@naver.com>
2020-05-06 15:49:20 +0900

    Added function
    1. member_info function
     - This function is searching of member's infomation

--- common/views.py
+++ common/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from member.models import Member
+from utils.functions import make_response
+
+
+@login_required
+def member_info(request):
+    members = Member.objects.all()
+    depts = members.values('dept').distinct().order_by('dept')
+
+    members_data = []
+
+    for dept in depts:
+        classify_member = members.filter(dept=dept['dept'])
+        data_format = {
+            'groupName': dept['dept'],
+            'groupData': []}
+
+        for member in classify_member:
+            data_format['groupData'].append(
+                {'name': member.name + " " + member.rank, 'value': member.id})
+        members_data.append(data_format)
+    return make_response(status=200, content=json.dumps({'members_data': members_data}))
