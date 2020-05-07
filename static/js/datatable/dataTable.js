d16416c519db277498bb0691e22ec7dae05d1308
sewookJung <tpdnrqkqh@naver.com>
2020-05-07 18:39:52 +0900

    Added document attach datatable

--- static/js/datatable/dataTable.js
+++ static/js/datatable/dataTable.js
@@ -48,4 +48,19 @@ $(document).ready(function () {
       },
     },
   });
+
+  $("#doument_attach").DataTable({
+    pageLength: 15,
+    lengthMenu: [5, 10, 15, 20, 30, 50, 100],
+    columnDefs: [
+      { orderable: true, targets: [0, 1] },
+      { orderable: false, targets: "_all" },
+    ],
+    language: {
+      paginate: {
+        previous: "‹",
+        next: "›",
+      },
+    },
+  });
 });
