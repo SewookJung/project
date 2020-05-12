e3fee38426fc4c7eb9c0122d9e6241b284ffdc95
sewookJung <tpdnrqkqh@naver.com>
2020-05-12 10:02:36 +0900

    [MODIFY] Weekly table and document_attach table

--- static/js/datatable/dataTable.js
+++ static/js/datatable/dataTable.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
     pageLength: 15,
     lengthMenu: [5, 10, 15, 20, 30, 50, 100],
     columnDefs: [
-      { orderable: true, targets: [0] },
+      { orderable: true, targets: [0, 1] },
       { orderable: false, targets: "_all" },
     ],
     language: {
@@ -53,7 +53,7 @@ $(document).ready(function () {
     pageLength: 15,
     lengthMenu: [5, 10, 15, 20, 30, 50, 100],
     columnDefs: [
-      { orderable: true, targets: [0, 1] },
+      { orderable: true, targets: [2] },
       { orderable: false, targets: "_all" },
     ],
     language: {
@@ -62,5 +62,6 @@ $(document).ready(function () {
         next: "›",
       },
     },
+    order: [[2, "dec"]],
   });
 });
