0aec05bb6661c3d593208c0a5e0778b656280c44
sewookJung <tpdnrqkqh@naver.com>
2020-05-15 10:06:38 +0900

    [ADD] member's info function

--- common/views.py
+++ common/views.py
@@ -26,3 +26,23 @@ def member_info(request):
                 {'name': member.name + " " + member.rank, 'value': member.id})
         members_data.append(data_format)
     return make_response(status=200, content=json.dumps({'members_data': members_data}))
+
+
+@login_required
+def member_info_all(request):
+    members = Member.objects.all()
+    depts = members.values('dept').distinct().order_by('dept')
+
+    members_data = []
+
+    for dept in depts:
+        classify_member = members.filter(dept=dept['dept'])
+        data_format = {
+            'groupName': dept['dept'],
+            'groupData': []}
+
+        for member in classify_member:
+            data_format['groupData'].append(
+                {'name': member.name + " " + member.rank, 'value': member.id})
+        members_data.append(data_format)
+    return members_data
