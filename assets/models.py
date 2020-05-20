d5087633e0a7ae70725209641010221d6fb86abc
sewookJung <tpdnrqkqh@naver.com>
2020-05-20 14:41:45 +0900

    [MODIFY] Increase charfield length

--- assets/models.py
+++ assets/models.py
@@ -11,20 +11,20 @@ class Asset(models.Model):
     # 'glowellsystem' - 테스트용, 폐기의 경우 변경된다.
     member_name = models.ForeignKey(
         Member, null=True, on_delete=models.SET_NULL)
-    mnfacture = models.CharField(max_length=30, default='')  # 제조사명
-    model = models.CharField(max_length=40, default='')  # 모델명
-    serial = models.CharField(max_length=40, default='')  # Serial
-    cpu = models.CharField(max_length=100, default='')  # CPU
-    memory = models.CharField(max_length=30, default='')  # MEMORY
-    harddisk = models.CharField(max_length=30, default='')  # HARDDISK
-    is_where = models.CharField(max_length=30, default='')  # 사용처
+    mnfacture = models.CharField(max_length=128, default='')  # 제조사명
+    model = models.CharField(max_length=128, default='')  # 모델명
+    serial = models.CharField(max_length=128, default='')  # Serial
+    cpu = models.CharField(max_length=128, default='')  # CPU
+    memory = models.CharField(max_length=128, default='')  # MEMORY
+    harddisk = models.CharField(max_length=128, default='')  # HARDDISK
+    is_where = models.CharField(max_length=128, default='')  # 사용처
     # 현재상태 1: 개인사용, 2: TEST, 3: 대여중, 4:폐기 5: 보관
     is_state = models.IntegerField(default=1)
     purchase_date = models.DateField(null=True, blank=True)  # 구입일
     created = models.DateTimeField(auto_now_add=True, editable=False)  # 등록날짜
     closed = models.DateField(null=True, blank=True)  # 폐기일
     comments = models.CharField(
-        max_length=200, default='', blank=True)  # 기타 comment
+        max_length=1024, default='', blank=True)  # 기타 comment
 
     def __str__(self):
         return self.model
