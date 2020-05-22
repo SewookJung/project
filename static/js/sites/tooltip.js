d9e09d7953ecf5a5202ea1fbb9cc4b81bfc16030
sewookJung <tpdnrqkqh@naver.com>
2020-05-22 09:49:56 +0900

    [REMOVE] tooltip function

--- static/js/sites/tooltip.js
+++ static/js/sites/tooltip.js
@@ -1,25 +1,13 @@
-$(function() {
-  $('[data-toggle="tooltip_wbs"]').tooltip({
-    title: "<div>[삼성디스플레이] WBS</div>",
-    html: true
-  });
-  $('[data-toggle="tooltip_confirm"]').tooltip({
-    title:
-      "<div>[삼성디스플레이] 검수확인서 1.0V</div><div>[삼성디스플레이] 검수확인서 1.1</div>",
-    html: true
-  });
-});
-
-$(document).ready(function() {
-  var content = [
+$(document).ready(function () {
+  const content = [
     '<div class="timePickerCanvas"><a href="#">NAC 제품소개서</a></div>',
     '<div class="timePickerClock timePickerHours"><a href="#">EDR 제품소개서</a></div>',
-    '<div class="timePickerClock timePickerMinutes"><a href="#">GPI 제품소개서</a></div>'
+    '<div class="timePickerClock timePickerMinutes"><a href="#">GPI 제품소개서</a></div>',
   ].join("");
 
   $('[data-toggle="popover_presales"]').popover({
     trigger: "focus",
     html: true,
-    content: content
+    content: content,
   });
 });
