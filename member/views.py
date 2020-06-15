f093e3b7f539f9b9bee266c9dca4282698d516e0
sewookJung <tpdnrqkqh@naver.com>
2020-06-15 16:47:06 +0900

    [ADD] To change password function

--- member/views.py
+++ member/views.py
@@ -7,3 +7,12 @@ def member_profile(request):
     member = Member.objects.get(id=request.session['id'])
     print(member)
     return render(request, "member/member_profile.html", {'member': member})
+
+
+def member_password(request):
+    return render(request, "member/member_password.html", {})
+
+
+def member_password_apply(request):
+    print(request.POST)
+    return render(request, "member/member_password.html", {})
