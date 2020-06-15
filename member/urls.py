c639a47a2b845aebee49a82112b6b203d347d1ed
sewookJung <tpdnrqkqh@naver.com>
2020-06-15 16:51:18 +0900

    [ADD] To change password urls

--- member/urls.py
+++ member/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.member_profile, name="member_profile"),
+    re_path(r'^password/$', views.member_password, name="member_password"),
+    re_path(r'^password/apply/$', views.member_password_apply,
+            name="member_password_apply"),
 ]
