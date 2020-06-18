a906d3d3da52c9350e95a132b3a34ed7eacbfa34
sewookjung <tpdnrqkqh@naver.com>
2020-06-18 16:49:10 +0900

    [ADD] equipment service

--- equipment/urls.py
+++ equipment/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     re_path(r'^$', views.equipment_main, name="equipment_main"),
     re_path(r'^form/', views.equipment_form, name="equipment_form"),
     re_path(r'^input/', views.equipment_input, name="equipment_input"),
-    re_path(r'^bulk/', views.equipment_bulk, name="equipment_bulk"),
-    re_path(r'^bulk_check/', views.equipment_bulk_check, name="equipment_bulk_check"),    
     re_path(r'^upload/', views.equipment_upload, name="equipment_upload"),
+    re_path(r'^upload_check/', views.equipment_upload_check, name="equipment_upload_check"),
+    re_path(r'^upload_complete/', views.equipment_upload_complete, name="equipment_upload_complete"),
+    re_path(r'^upload_cancel/', views.equipment_upload_cancel, name="equipment_upload_cancel"),
 ]
