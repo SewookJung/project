48f24450736c107194e7fad4e3de289eaac27616
sewookJung <tpdnrqkqh@naver.com>
2020-07-13 17:49:29 +0900

    [ADD] quipment

---
+++ static/js/equipment/info.js
@@ -0,0 +1,50 @@
+function clientSearchFunction() {
+  let input = document.getElementById("client-search");
+  let filter = input.value.toUpperCase();
+  let ul = document.getElementById("clientUL");
+  let li = ul.getElementsByTagName("li");
+  let i, txtValue;
+
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function mnfactureSearchFunction() {
+  let input = document.getElementById("mnfacture-search");
+  let filter = input.value.toUpperCase();
+  let ul = document.getElementById("mnfactureUL");
+  let li = ul.getElementsByTagName("li");
+  let i, txtValue;
+
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function productSearchFunction() {
+  let input = document.getElementById("product-model-search");
+  let filter = input.value.toUpperCase();
+  let ul = document.getElementById("productModelUL");
+  let li = ul.getElementsByTagName("li");
+  let i, txtValue;
+
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "block";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
