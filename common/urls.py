703c6f76e0ffde58aa2ece1123cd61a1b30c3793
sewookJung <tpdnrqkqh@naver.com>
2020-07-16 15:38:27 +0900

    [ADD] check the duplication client and add new client urls

--- common/urls.py
+++ common/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     re_path(r'^member/info/$', views.member_info, name="member_info"),
     re_path(r'^weekly/permission/$', views.weekly_permission,
             name="weekly_permission"),
+    re_path(r'^client/dup/check/$', views.client_dup_check,
+            name="client_dup_check"),
+    re_path(r'^client/add/apply/$', views.client_add_apply,
+            name="client_add_apply"),
 ]
