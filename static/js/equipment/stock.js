a78bc9e88ab23449dba967e7a1f0cd8068d6600e
sewookJung <tpdnrqkqh@naver.com>
2020-08-09 14:09:10 +0900

    [ADD] Renewaled modal contents when click the apply and delete button click

--- static/js/equipment/stock.js
+++ static/js/equipment/stock.js
@@ -1,16 +1,46 @@
 const csrfToken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+const modalContent = document.getElementById("modal-content");
+const waringContent = document.getElementById("waring-content");
+const multiContent = document.getElementById("multi-delivery");
+const singleContent = document.getElementById("single-delivery");
+const accordion = document.getElementById("accordion");
 
-const stockApply = (stockId, mnfacture, product, product_model, serial) => {
+let selectedStockIds = new Array();
+let stockInfoOjbect = new Object();
+let stockInfoResult = new Object();
+let rowData;
+let checkBox;
+let serial;
+let mnfacture;
+let deleteStockObject;
+let checkBoxStatus;
+
+const stockApply = (
+  stockId,
+  mnfacture,
+  product,
+  productModel,
+  serial,
+  receiveDate
+) => {
   const stockIdInput = document.getElementById("stock_id");
-  const mnfactureInput = document.getElementById("mnfacture");
-  const productInput = document.getElementById("product");
-  const productModelInput = document.getElementById("product-model");
-  const serialInput = document.getElementById("serial");
+  const titleOfMnfacture = document.getElementById("media-title");
+  const liOfProduct = document.getElementById("stock-product");
+  const liOfProductModel = document.getElementById("stock-product-model");
+  const liOfSerial = document.getElementById("stock-serial");
+  const liOfReceiveDate = document.getElementById("stock-receive-date");
+  const submitBtn = document.getElementById("sumbit-btn");
+  submitBtn.setAttribute("onclick", "valueCheck()");
+  modalContent.style.display = "block";
+  waringContent.style.display = "none";
+  multiContent.style.display = "none";
+  singleContent.style.display = "block";
   stockIdInput.value = stockId;
-  mnfactureInput.value = mnfacture;
-  productInput.value = product;
-  productModelInput.value = product_model;
-  serialInput.value = serial;
+  titleOfMnfacture.innerText = mnfacture;
+  liOfProduct.innerText = product;
+  liOfProductModel.innerText = productModel;
+  liOfSerial.innerText = serial;
+  liOfReceiveDate.innerText = receiveDate + " 입고";
 };
 
 const valueCheck = () => {
@@ -70,22 +100,300 @@ const valueCheck = () => {
   });
 };
 
-function checkTheBox(event) {
+const checkTheBox = (event) => {
   const clickedElementId = event.target.id;
-  if (clickedElementId == "blankCheckbox") return false;
+  stockInfoOjbect = new Object();
+  switch (clickedElementId) {
+    case "blankCheckbox":
+      checkBox = event.target;
+      checkBoxStatus = checkBox.checked;
+      rowData = event.target.parentNode.parentNode.children;
+      if (checkBoxStatus == true) {
+        mnfacture = rowData[1].children[0].innerText;
+        if (!(mnfacture in stockInfoResult)) {
+          stockInfoResult[mnfacture] = new Array();
+        }
+        stockInfoOjbect.id = checkBox.value;
+        stockInfoOjbect.product = rowData[2].innerText;
+        stockInfoOjbect.model = rowData[3].innerText;
+        stockInfoOjbect.serial = rowData[4].innerText;
+        stockInfoResult[mnfacture].push(stockInfoOjbect);
+        selectedStockIds.push(stockInfoOjbect.id);
+      } else {
+        serial = rowData[4].innerText;
+        deleteStockObject = stockInfoResult[mnfacture].findIndex(function (
+          item
+        ) {
+          return item.serial === serial;
+        });
+        if (deleteStockObject > -1)
+          stockInfoResult[mnfacture].splice(deleteStockObject, 1);
+        if (stockInfoResult[mnfacture].length < 1)
+          delete stockInfoResult[mnfacture];
 
-  if (clickedElementId == "form-check") {
-    const checkBox = event.target.childNodes[1];
-    if (checkBox.checked == false) {
-      checkBox.checked = true;
-      return false;
-    } else {
-      checkBox.checked = false;
-      return false;
+        const selectReleaseStock = selectedStockIds.findIndex(function (item) {
+          return item === checkBox.value;
+        });
+        if (selectReleaseStock > -1) {
+          selectedStockIds.splice(selectReleaseStock, 1);
+        }
+      }
+      break;
+
+    case "td-checkbox":
+      checkBox = event.target.childNodes[1];
+      checkBoxStatus = checkBox.checked;
+      rowData = event.target.parentNode.children;
+      if (checkBox.checked == false) {
+        checkBox.checked = true;
+        mnfacture = rowData[1].children[0].innerText;
+        if (!(mnfacture in stockInfoResult)) {
+          stockInfoResult[mnfacture] = new Array();
+        }
+        stockInfoOjbect.id = checkBox.value;
+        stockInfoOjbect.product = rowData[2].innerText;
+        stockInfoOjbect.model = rowData[3].innerText;
+        stockInfoOjbect.serial = rowData[4].innerText;
+        stockInfoResult[mnfacture].push(stockInfoOjbect);
+        selectedStockIds.push(stockInfoOjbect.id);
+      } else {
+        checkBox.checked = false;
+        serial = rowData[4].innerText;
+        deleteStockObject = stockInfoResult[mnfacture].findIndex(function (
+          item
+        ) {
+          return item.serial === serial;
+        });
+        if (deleteStockObject > -1)
+          stockInfoResult[mnfacture].splice(deleteStockObject, 1);
+        if (stockInfoResult[mnfacture].length < 1)
+          delete stockInfoResult[mnfacture];
+        const selectReleaseStock = selectedStockIds.findIndex(function (item) {
+          return item === checkBox.value;
+        });
+        if (selectReleaseStock > -1) {
+          selectedStockIds.splice(selectReleaseStock, 1);
+        }
+      }
+      break;
+  }
+};
+
+const deleteCards = () => {
+  const foo = accordion.querySelectorAll(".card");
+  if (foo.length > 0) {
+    for (let i = 0; i < foo.length; i++) {
+      foo[i].remove();
     }
   }
+};
 
-  const checkBox = event.target.childNodes[1].childNodes[1];
-  if (checkBox.checked == false) checkBox.checked = true;
-  else checkBox.checked = false;
-}
+const checkSelectedStocks = () => {
+  const selectedStocklength = Object.keys(stockInfoResult).length;
+  deleteCards();
+  if (selectedStocklength > 0) {
+    multiContent.style.display = "block";
+    modalContent.style.display = "block";
+    waringContent.style.display = "none";
+    singleContent.style.display = "none";
+    const selectedStockKeys = Object.keys(stockInfoResult);
+
+    selectedStockKeys.forEach(function (mnfacture) {
+      const card = document.createElement("div");
+      const cardHeader = document.createElement("div");
+      const anchor = document.createElement("a");
+      const collapseDiv = document.createElement("div");
+      const cardBody = document.createElement("div");
+      const ul = document.createElement("ul");
+      const span = document.createElement("span");
+      const selectedStocks = stockInfoResult[mnfacture];
+      const submitBtn = document.getElementById("sumbit-btn");
+      submitBtn.setAttribute("onclick", "multiStocksApply()");
+
+      card.classList.add("card");
+      cardHeader.classList.add("card-header");
+      anchor.classList.add("card-link");
+      anchor.dataset.toggle = "collapse";
+      anchor.innerText = mnfacture;
+      anchor.href = "#" + mnfacture;
+      collapseDiv.id = mnfacture;
+      collapseDiv.classList.add("collapse");
+      cardBody.classList.add("card-body");
+      ul.classList.add("list-group");
+      span.classList.add("badge");
+      span.classList.add("badge-dark");
+
+      card.appendChild(cardHeader);
+      cardHeader.append(anchor);
+      cardHeader.append(span);
+      card.appendChild(collapseDiv);
+      collapseDiv.appendChild(cardBody);
+      cardBody.appendChild(ul);
+
+      for (let i = 0; i < selectedStocks.length; i++) {
+        const li = document.createElement("li");
+        const stockInfo =
+          selectedStocks[i].product +
+          "  " +
+          selectedStocks[i].model +
+          "  " +
+          selectedStocks[i].serial;
+        li.classList.add("list-group-item");
+        li.innerText = stockInfo;
+        ul.append(li);
+      }
+      span.innerText = selectedStocks.length;
+      accordion.append(card);
+    });
+  } else {
+    waringContent.style.display = "block";
+    modalContent.style.display = "none";
+  }
+};
+
+const multiStocksApply = () => {
+  const client = document.getElementById("client_id");
+  const deliveryDate = document.getElementById("equipment-install-date");
+  const manager = document.getElementById("manager");
+  const location = document.getElementById("location");
+  const clientVlaue = client.options[client.selectedIndex].value;
+  const deliveryDateValue = deliveryDate.value;
+  const managerValue = manager.value;
+  const locationValue = location.value;
+
+  if (clientVlaue == "") {
+    alert("❗ 납품 고객사를 선택해주세요.");
+    return false;
+  }
+
+  if (deliveryDateValue == "") {
+    alert("❗ 납품 날짜를 선택해주세요.");
+    return false;
+  }
+
+  if (managerValue == "") {
+    alert("❗ 납품 장비 담당자를 작성해주세요.");
+    return false;
+  }
+
+  if (locationValue == "") {
+    alert("❗ 납품 장소를 작성해주세요.");
+    return false;
+  }
+
+  param = {};
+  param.client = clientVlaue;
+  param.deliveryDate = deliveryDateValue;
+  param.manager = managerValue;
+  param.location = locationValue;
+  param.stockIds = selectedStockIds;
+
+  $.ajax({
+    url: "/equipment/stock/multi/apply/",
+    data: param,
+    type: "POST",
+    dataType: "json",
+    headers: { "X-CSRFToken": csrfToken },
+    success: function (data) {
+      const successMsg = data.msg;
+      alert(successMsg);
+      window.location = "/equipment/";
+    },
+    error: function (request, status, error) {
+      const errorMsg = JSON.parse(request.responseText).msg;
+      alert(errorMsg);
+      window.location = "/equipment/stock/";
+    },
+  });
+};
+
+const deleteStocks = () => {
+  const selectedStocklength = Object.keys(stockInfoResult).length;
+  const deleteSelectErrorContent = document.getElementById(
+    "delete-select-error"
+  );
+  const deleteSuccessContent = document.getElementById("delete-success");
+  const deletePermissionErrorContent = document.getElementById(
+    "delete-permission-denied"
+  );
+
+  if (selectedStocklength > 0) {
+    param = {};
+    param.stockIds = selectedStockIds;
+
+    $.ajax({
+      url: "/equipment/stock/permission/check/",
+      data: param,
+      type: "POST",
+      dataType: "json",
+      headers: { "X-CSRFToken": csrfToken },
+      success: function (data) {
+        deleteSelectErrorContent.style.display = "none";
+        deletePermissionErrorContent.style.display = "none";
+        deleteSuccessContent.style.display = "block";
+      },
+      error: function (request, status, error) {
+        const deniedStockLists = document.getElementById("denied-stock-ul");
+        const permissionDeniedContent = document.getElementById(
+          "permission-denied-body"
+        );
+
+        if (deniedStockLists != null) {
+          deniedStockLists.remove();
+        }
+
+        const ul = document.createElement("ul");
+        ul.classList.add("list-group");
+        ul.id = "denied-stock-ul";
+
+        permissionDeniedContent.append(ul);
+        deleteSelectErrorContent.style.display = "none";
+        deleteSuccessContent.style.display = "none";
+        deletePermissionErrorContent.style.display = "block";
+
+        const deniedLists = JSON.parse(request.responseText).denied_lists;
+        for (let i = 0; i < deniedLists.length; i++) {
+          const li = document.createElement("li");
+          const stockInfo =
+            deniedLists[i].product +
+            "  " +
+            deniedLists[i].product_model +
+            "  " +
+            deniedLists[i].serial +
+            "\n   🙍‍♂️" +
+            deniedLists[i].creator;
+          li.classList.add("list-group-item");
+          li.innerText = stockInfo;
+          ul.append(li);
+        }
+        permissionDeniedContent.append(ul);
+      },
+    });
+  } else {
+    deletePermissionErrorContent.style.display = "none";
+    deleteSuccessContent.style.display = "none";
+    deleteSelectErrorContent.style.display = "block";
+  }
+};
+
+const stockMultiDelete = () => {
+  param = {};
+  param.stockIds = selectedStockIds;
+  $.ajax({
+    url: "/equipment/stock/multi/delete/",
+    data: param,
+    type: "POST",
+    dataType: "json",
+    headers: { "X-CSRFToken": csrfToken },
+    success: function (data) {
+      const successMsg = data.msg;
+      alert(successMsg);
+      window.location = "/equipment/stock/";
+    },
+    error: function (request, status, error) {
+      const errorMsg = JSON.parse(request.responseText).msg;
+      alert(errorMsg);
+      window.location = "/equipment/stock/";
+    },
+  });
+};
