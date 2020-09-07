19cc3654f36f9e0d56f1d5c7334d33eb3ec7752e
sewookJung <tpdnrqkqh@naver.com>
2020-09-07 11:18:36 +0900

    [ADD] client selectbox attribute

--- equipment/forms.py
+++ equipment/forms.py
@@ -8,7 +8,7 @@ class EquipmentForm(forms.ModelForm):
         model = Equipment
         fields = ('client', 'product_model', 'mnfacture')
         widgets = {
-            'client': forms.Select(attrs={'class': 'selectpicker', 'title': "고객사", 'id': 'client_id', 'data-live-search': 'true'}),
+            'client': forms.Select(attrs={'class': 'selectpicker', 'title': "고객사", 'id': 'client_id', 'data-live-search': 'true', 'data-container': 'body'}),
             'product_model': forms.Select(attrs={'class': 'selectpicker', 'title': "제조사", 'id': 'product-model', 'data-live-search': 'true'}),
             'mnfacture': forms.Select(attrs={'class': 'selectpicker', 'title': "모델명", 'id': 'mnfacture', 'data-live-search': 'true', 'data-container': 'body'}),
         }
