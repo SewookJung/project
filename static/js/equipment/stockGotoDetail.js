91fccf4287271d04b13d19f9219905b721e0edda
sewookJung <tpdnrqkqh@naver.com>
2020-09-22 11:12:09 +0900

    [ADD] go to all page of mnfacture stock

--- static/js/equipment/stockGotoDetail.js
+++ static/js/equipment/stockGotoDetail.js
@@ -3,3 +3,7 @@ const goToDetailPage = (event, productId, status) => {
   if (selectedStockCount < 1) alert("선택한 목록이 존재하지 않습니다.");
   else window.location = `/equipment/stock/${productId}/${status}`;
 };
+
+const goToAllStockPage = (mnfacture) => {
+  window.location = `/equipment/stock/${mnfacture}/all/detail/`;
+};
