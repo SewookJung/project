4ace4597390f7d83881d60046747611cf12992e8
sewookJung <tpdnrqkqh@naver.com>
2020-09-22 11:05:34 +0900

    [ADD] get product model when select manafacture selected

---
+++ static/js/equipment/productModelLists.js
@@ -0,0 +1,29 @@
+const mnfactureSelectBox = document.getElementById("mnfacture");
+
+mnfactureSelectBox.addEventListener("change", (event) => {
+  mnfactureId = event.target.value;
+
+  if (!mnfactureId) return;
+  else {
+    $.ajax({
+      url: `/common/get/model/${mnfactureId}/lists/`,
+      type: "GET",
+      dataType: "json",
+      success: function (data) {
+        const productModels = data.product_models;
+        $("#product-model").find("option").remove();
+        $("#product-model").selectpicker({ title: "모델명 선택" });
+        for (let i = 0; i < productModels.length; i++) {
+          const id = productModels[i]["id"];
+          const name = productModels[i]["name"];
+          $("#product-model").append(`<option value="${id}">${name}</option>`);
+        }
+        $("#product-model").selectpicker("refresh");
+      },
+      error: function (request, status, error) {
+        const errorMsg = JSON.parse(request.responseText).msg;
+        alert(errorMsg);
+      },
+    });
+  }
+});
