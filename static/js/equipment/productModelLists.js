89cebf2e775d3659e807ea777a58fc86e75d7eff
sewookJung <tpdnrqkqh@naver.com>
2020-09-28 10:55:51 +0900

    [ADD] If detail page get list call the get product model list function

--- static/js/equipment/productModelLists.js
+++ static/js/equipment/productModelLists.js
@@ -27,3 +27,27 @@ mnfactureSelectBox.addEventListener("change", (event) => {
     });
   }
 });
+
+const getProductModelLists = (mnfactureId, modelId) => {
+  $.ajax({
+    url: `/common/get/model/${mnfactureId}/lists/`,
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      const productModels = data.product_models;
+      $("#product-model").find("option").remove();
+      $("#product-model").selectpicker({ title: "모델명 선택" });
+      for (let i = 0; i < productModels.length; i++) {
+        const id = productModels[i]["id"];
+        const name = productModels[i]["name"];
+        $("#product-model").append(`<option value="${id}">${name}</option>`);
+      }
+      $("#product-model").val(modelId);
+      $("#product-model").selectpicker("refresh");
+    },
+    error: function (request, status, error) {
+      const errorMsg = JSON.parse(request.responseText).msg;
+      alert(errorMsg);
+    },
+  });
+};
