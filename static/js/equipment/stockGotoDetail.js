b09e9ea3f4e78e317fcf12d79727c93ff6159538
sewookJung <tpdnrqkqh@naver.com>
2020-10-29 17:13:49 +0900

    [MODIFY] changed selected stock count

--- static/js/equipment/stockGotoDetail.js
+++ static/js/equipment/stockGotoDetail.js
@@ -1,5 +1,7 @@
 const goToDetailPage = (event, productId, status) => {
-  const selectedStockCount = Number(event.target.innerText);
+  const selectedTd = event.target.innerText;
+  const selectedStockCount = Number(selectedTd.charAt(0));
+
   if (selectedStockCount < 1) alert("선택한 목록이 존재하지 않습니다.");
   else window.location = `/equipment/stock/${productId}/${status}`;
 };
