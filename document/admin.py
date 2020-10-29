31c66b5d025d5eb150a617d3f03a56038882bc09
sewookJung <tpdnrqkqh@naver.com>
2020-10-29 16:22:06 +0900

    [REMOVE] Removed document attach

--- document/admin.py
+++ document/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'client', 'project', 'creator', 'created_at', 'comments', ]
+    list_display_links = ['id', 'client']
+
+
+admin.site.register(Document, DocumentAdmin)
