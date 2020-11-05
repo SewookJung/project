c3749376e5d3cad745c466518a239569f0fd80f3
sewookJung <tpdnrqkqh@naver.com>
2020-11-05 13:55:38 +0900

    [MODIFY] Functions name changed

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.login_main, name="login_main"),
     url(r'^check/$', views.login_process, name="login_process"),
-    url(r'^logout/$', views.logout, name="logout"),
-    url(r'^', views.login, name="login"),
+    url(r'^logout/$', views.logout_process, name="logout_process"),
 ]
