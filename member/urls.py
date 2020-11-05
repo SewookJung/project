ea33499544aaf5fc58f95803e14c7632ed91c30b
sewookJung <tpdnrqkqh@naver.com>
2020-11-05 14:02:28 +0900

    [REMOVE] change the password function and urls

--- member/urls.py
+++ member/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.member_profile, name="member_profile"),
-    re_path(r'^password/$', views.member_password, name="member_password"),
-    re_path(r'^password/apply/$', views.member_password_apply,
-            name="member_password_apply"),
+    
 ]
